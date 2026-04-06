000d6256905ce45e912bb551354a1a61d9ecf81e
caianesantos <caianesantos.ph@gmail.com>
2026-04-06 13:46:36 -0300

    Adicionando enunciado da atividade

--- src/progressaoAritmetica/Main.java
+++ src/progressaoAritmetica/Main.java
@@ -1,3 +1,15 @@
+/***
+ * Implemente uma classe chamada ProgressaoAritmetica que possui três atributos
+ * (primeiroTermo, razao, termoAtual) e um método chamado proximoTermo que
+ * devolve o termo atual para quem chamou e calcula o próximo termo atualizando o
+ * termoAtual. A classe deverá ter um método construtor que receba o valor do primeiro
+ * termo e a razão e instancie o objeto com esses valores. Em seguida crie uma aplicação
+ * que instancie um objeto da classe implementada, e mostre na tela os n primeiros
+ * termos da progressão, sendo n definido pelo usuário. Em seguida a aplicação deve
+ * reinicializar o objeto estabelecendo novos valores para o primeiro termo e a razão
+ * (Deves ser usados os getters e setters para fazer isso).
+ */
+
 package progressaoAritmetica;
 
 import java.util.Scanner;

b568e14b5c45a8253da72436f2c84ed5af7795f2
caianesantos <caianesantos.ph@gmail.com>
2026-04-06 13:43:02 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ProgressaoAritmetica pa = new ProgressaoAritmetica(5);
+        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a quantidade de termos: ");
+        int termo = sc.nextInt();
+
+        for(int i = 0; i <= termo; i++) {
+            System.out.println(pa.toString());
+        }
+    }
+}
---
+++ src/ProgressaoAritmetica.java
@@ -0,0 +1,24 @@
+public class ProgressaoAritmetica {
+    private int primeiroTermo;
+    private int razao;
+    private int termoAtual;
+
+    public ProgressaoAritmetica(int termoAtual){
+        this.primeiroTermo = 2;
+        this.razao = 4;
+        this.termoAtual = termoAtual;
+    }
+    public int proximoTermo(){
+        int proximoTermo = this.termoAtual;
+        termoAtual += this.razao;
+        return this.termoAtual;
+    }
+
+    public int getProgressaoAritmetica(){
+        return this.termoAtual;
+    }
+
+    public String toString(){
+        return "1° termo: " + this.primeiroTermo + " razão: " + this.razao + " termo atual: " + proximoTermo();
+    }
+}
